897ec81d0b45a2ebce69f6dd425d6f62f4f9620c
Edward Hong <edward.hong527@gmail.com>
2017-11-04 01:48:34 +1300

    add login modal

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,25 +5,48 @@ import {
 	NavbarToggler,
 	NavbarBrand,
 	Nav,
-	NavItem
+	NavItem,
+	NavLink,
+	Modal,
+	ModalBody,
+	Button
 } from 'reactstrap'
 
 export default class Header extends Component {
 	state = {
-		isOpen: false
+		isOpen: false,
+		modal: false
 	}
 	toggle = () => this.setState({ isOpen: !this.state.isOpen })
+	toggleModal = () => this.setState({ modal: !this.state.modal })
 	render() {
 		return (
-			<Navbar light expand="md">
-				<NavbarBrand href="/">VoteMole</NavbarBrand>
-				<NavbarToggler onClick={this.toggle} />
-				<Collapse isOpen={this.state.isOpen} navbar>
-					<Nav className="ml-auto" navbar>
-						<NavItem>Login</NavItem>
-					</Nav>
-				</Collapse>
-			</Navbar>
+			<div>
+				<Navbar light expand="md">
+					<NavbarBrand href="/">VoteMole</NavbarBrand>
+					<NavbarToggler onClick={this.toggle} />
+					<Collapse isOpen={this.state.isOpen} navbar>
+						<Nav className="ml-auto" navbar>
+							<NavLink href="#" onClick={this.toggleModal}>
+								<NavItem>Login</NavItem>
+							</NavLink>
+						</Nav>
+					</Collapse>
+				</Navbar>
+				<Modal isOpen={this.state.modal} toggle={this.toggleModal}>
+					<ModalBody>
+						<Button className="btn-facebook" outline block>
+							Login with Facebook
+						</Button>
+						<Button className="btn-google" outline block>
+							Login with Google
+						</Button>
+						<Button className="btn-github" outline block>
+							Login with Github
+						</Button>
+					</ModalBody>
+				</Modal>
+			</div>
 		)
 	}
 }
