959ccf131f5e83dd8611c6f073c4ddc0e9e5f2bf
Edward Hong <edward.hong527@gmail.com>
2017-11-03 18:46:12 +1300

    add header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 
+import Header from './Header'
+
 const Landing = () => <div>Landing Page</div>
 
 const App = () => (
 	<BrowserRouter>
 		<div>
+			<Header />
 			<Route path="/" component={Landing} />
 		</div>
 	</BrowserRouter>
---
+++ client/src/components/Header.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import {
+	Collapse,
+	Navbar,
+	NavbarToggler,
+	NavbarBrand,
+	Nav,
+	NavItem,
+	NavLink
+} from 'reactstrap'
+
+export default class Header extends Component {
+	state = {
+		isOpen: false
+	}
+	toggle = () => this.setState({ isOpen: !this.state.isOpen })
+	render() {
+		return (
+			<Navbar color="faded" light expand="md">
+				<NavbarBrand href="/">VoteMole</NavbarBrand>
+				<NavbarToggler onClick={this.toggle} />
+				<Collapse isOpen={this.state.isOpen} navbar>
+					<Nav className="ml-auto" navbar>
+						<NavItem>Login</NavItem>
+					</Nav>
+				</Collapse>
+			</Navbar>
+		)
+	}
+}
