91b5fa31bdb59ee51caea6f7ad1956a64a9e963c
Edward Hong <edward.hong527@gmail.com>
2017-11-05 02:44:44 +1300

    refactor auth actions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,14 +1,3 @@
-import axios from 'axios'
+import { fetchUser } from './authActions'
 
-import { FETCH_USER } from './types'
-
-export const fetchUser = () => dispatch => {
-	axios('/auth/current_user').then(res => {
-		if (res.status === 200) {
-			dispatch({
-				type: FETCH_USER,
-				payload: res.data
-			})
-		}
-	})
-}
+export { fetchUser }
