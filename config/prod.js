9ee648f7cbfc7b242b9c6c2b5da16cc5f5dffa88
Edward Hong <edward.hong527@gmail.com>
2017-11-04 21:02:15 +1300

    add google, facebook and github client IDs and secret keys

--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,9 @@
 module.exports = {
-	mongoURI: process.env.MONGO_URI
+	mongoURI: process.env.MONGO_URI,
+	googleClientID: process.env.GOOGLE_CLIENT_ID,
+	googleSecretKey: process.env.GOOGLE_SECRET_KEY,
+	facebookClientID: process.env.FACEBOOK_CLIENT_ID,
+	facebookSecretKey: process.env.FACEBOOK_SECRET_KEY,
+	githubClientID: process.env.GITHUB_CLIENT_ID,
+	githubSecretKey: process.env.GITHUB_SECRET_KEY
 }
