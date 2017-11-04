441fa48a19a46b42315a6e4c1985c9749faa685f
Edward Hong <edward.hong527@gmail.com>
2017-11-04 21:15:24 +1300

    add boilerplate passport, express and body-parser code to index.js

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const passport = require('passport')
+const session = require('express-session')
+const bodyParser = require('body-parser')
 
 const keys = require('./config/keys')
+
+require('./models/User')
 
 mongoose.connect(keys.mongoURI, { useMongoClient: true })
 mongoose.Promise = global.Promise
 
 const app = express()
+
+app.use(bodyParser.json())
+
+app.use(
+	session({
+		secret: keys.cookieKey,
+		saveUninitialized: true,
+		resave: true
+	})
+)
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
