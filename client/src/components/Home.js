9d76bbdae263b1d457941bd182c06cb2a52e0a58
Edward Hong <edward.hong527@gmail.com>
2017-11-06 02:13:16 +1300

    add heading and logo in home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,3 +1,26 @@
 import React from 'react'
+import { Container, Row, Col } from 'reactstrap'
 
-export default () => <div>Home</div>
+const Home = () => {
+	return (
+		<Container fluid>
+			<h1 className="text-center">VoteMole</h1>
+			<h2 className="text-center">Can You Dig It?</h2>
+			<Row className="logo-row">
+				<Col
+					lg={{ size: '6', offset: 3 }}
+					md={{ size: '8', offset: 2 }}
+					sm={{ size: '10', offset: 1 }}
+					xs="12">
+					<img
+						className="logo-home"
+						src="https://res.cloudinary.com/avatarhzh/image/upload/v1509887327/build-a-voting-app/logo.svg"
+						alt="Vote Mole logo"
+					/>
+				</Col>
+			</Row>
+		</Container>
+	)
+}
+
+export default Home
