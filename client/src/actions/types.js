8cb5bde6b8153d1130c2806af9d15893f383617b
Edward Hong <edward.hong527@gmail.com>
2017-11-06 02:31:15 +1300

    add all poll action type

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -3,3 +3,5 @@ export const FETCH_USER = 'fetch_user'
 export const TOGGLE_DROPDOWN = 'toggle_dropdown'
 export const TOGGLE_LOGIN_MODAL = 'toggle_login_modal'
 export const TOGGLE_POLL_MODAL = 'toggle_poll_modal'
+
+export const POLL_ALL = 'poll_all'
