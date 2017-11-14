5e0f44bfce90ee66a9235e4ca4dda04c439fb1c1
Edward Hong <edward.hong527@gmail.com>
2017-11-15 06:50:27 +1300

    refactor auth actions parameters

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -3,11 +3,11 @@ import axios from 'axios'
 import { FETCH_USER } from './types'
 
 export const fetchUser = () => dispatch => {
-	axios('/auth/current_user').then(res => {
-		if (res.status === 200) {
+	axios('/auth/current_user').then(({ status, data }) => {
+		if (status === 200) {
 			dispatch({
 				type: FETCH_USER,
-				payload: res.data
+				payload: data
 			})
 		}
 	})
