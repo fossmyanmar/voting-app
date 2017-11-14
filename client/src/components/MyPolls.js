3852e771236b158ac70b8480077a729b68e3426d
Edward Hong <edward.hong527@gmail.com>
2017-11-15 07:10:20 +1300

    connect MyPolls component to reduxstate and actions

--- client/src/components/MyPolls.js
+++ client/src/components/MyPolls.js
@@ -1,3 +1,14 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-export default () => <div>My Polls</div>
+import * as actions from '../actions'
+
+class MyPolls extends Component {
+	render() {
+		return <div>My Polls</div>
+	}
+}
+
+const mapStateToProps = ({ auth, poll }) => ({ auth, poll })
+
+export default connect(mapStateToProps, actions)(MyPolls)
