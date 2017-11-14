e28b3d5cb565bc4e3a521f4c7f598a363f5a36b9
Edward Hong <edward.hong527@gmail.com>
2017-11-15 06:48:40 +1300

    refactor poll actions parameters

--- client/src/actions/pollActions.js
+++ client/src/actions/pollActions.js
@@ -3,22 +3,22 @@ import axios from 'axios'
 import { POLL_ALL, POLL_DETAILS } from './types'
 
 export const allPolls = () => dispatch => {
-	axios.get('/poll/all_polls').then(res => {
-		if (res.status === 200) {
+	axios.get('/poll/all_polls').then(({ status, data }) => {
+		if (status === 200) {
 			dispatch({
 				type: POLL_ALL,
-				payload: res.data
+				payload: data
 			})
 		}
 	})
 }
 
 export const getPoll = id => dispatch => {
-	axios.get(`/poll/get_poll/${id}`).then(res => {
-		if (res.status === 200) {
+	axios.get(`/poll/get_poll/${id}`).then(({ status, data }) => {
+		if (status === 200) {
 			dispatch({
 				type: POLL_DETAILS,
-				payload: res.data
+				payload: data
 			})
 		}
 	})
