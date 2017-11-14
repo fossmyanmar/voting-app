39d52cf6d2d60808b7fc2912e6f178a9850a5390
Edward Hong <edward.hong527@gmail.com>
2017-11-15 06:52:21 +1300

    add poll user case to poll reducer

--- client/src/reducers/pollReducer.js
+++ client/src/reducers/pollReducer.js
@@ -1,10 +1,12 @@
-import { POLL_ALL, POLL_DETAILS } from '../actions/types'
+import { POLL_ALL, POLL_DETAILS, POLL_USER } from '../actions/types'
 
 export default (state = null, action) => {
 	switch (action.type) {
 		case POLL_ALL:
 		case POLL_DETAILS:
 			return action.payload || false
+		case POLL_USER:
+			return action.payload
 		default:
 			return state
 	}
