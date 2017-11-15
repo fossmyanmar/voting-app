514c836ae90287e9059f943e6453db5b6d22da75
Edward Hong <edward.hong527@gmail.com>
2017-11-16 07:50:09 +1300

    add RenderSelect presentational component

---
+++ client/src/components/presentational/RenderSelect.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { FormGroup, Input, Label, ButtonGroup, Button } from 'reactstrap'
+
+export default ({
+	input,
+	label,
+	type,
+	pollOptions,
+	meta: { touched, error }
+}) => {
+	let valid
+	touched ? (error ? (valid = false) : (valid = true)) : (valid = null)
+
+	const renderOptions = () => {
+		return pollOptions.map(({ name }, i) => <option key={i}>{name}</option>)
+	}
+
+	return (
+		<FormGroup>
+			<Label htmlFor={input.name}>{label}</Label>
+			<Input valid={valid} {...input} type={type}>
+				<option disabled />
+				{renderOptions()}
+			</Input>
+			{touched && error && <div className="error">{error}</div>}
+			<ButtonGroup vertical>
+				<Button outline color="success">
+					Submit
+				</Button>
+			</ButtonGroup>
+		</FormGroup>
+	)
+}
