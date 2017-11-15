d763f73327919400187925b3ee75ae593be0b270
Edward Hong <edward.hong527@gmail.com>
2017-11-16 05:34:08 +1300

    add hot loading

--- client/src/index.js
+++ client/src/index.js
@@ -14,10 +14,29 @@ import reducers from './reducers'
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk, logger))
 
+if (process.env.NODE_ENV !== 'production') {
+	if (module.hot) {
+		module.hot.accept('./reducers', () => {
+			store.replaceReducer(reducers)
+		})
+	}
+}
+
 render(
 	<Provider store={store}>
 		<App />
 	</Provider>,
 	document.querySelector('#root')
 )
+
+if (module.hot) {
+	module.hot.accept('./components/App', () => {
+		render(
+			<Provider store={store}>
+				<App />
+			</Provider>,
+			document.querySelector('#root')
+		)
+	})
+}
 registerServiceWorker()
