0b4ca1f12fd04708915cc5538a41e2c92074cae5
Edward Hong <edward.hong527@gmail.com>
2017-11-19 11:39:58 +1300

    add toggle delete modal action type

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -3,6 +3,7 @@ export const FETCH_USER = 'fetch_user'
 export const TOGGLE_DROPDOWN = 'toggle_dropdown'
 export const TOGGLE_LOGIN_MODAL = 'toggle_login_modal'
 export const TOGGLE_POLL_MODAL = 'toggle_poll_modal'
+export const TOGGLE_DELETE_MODAL = 'toggle_delete_modal'
 
 export const POLL_ALL = 'poll_all'
 export const POLL_DETAILS = 'poll_details'
